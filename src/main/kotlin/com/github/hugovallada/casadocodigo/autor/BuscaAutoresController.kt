1a47a3a57c93acbb69515d3e558d9a99ee1e888e
hugovallada <valladahugo@gmail.com>
2021-05-24 13:34:44 -0300

    Métodos que geram queries no Micronaut Data + uso de programação funcional do controller de Busca

--- src/main/kotlin/com/github/hugovallada/casadocodigo/autor/BuscaAutoresController.kt
+++ src/main/kotlin/com/github/hugovallada/casadocodigo/autor/BuscaAutoresController.kt
@@ -3,18 +3,54 @@ package com.github.hugovallada.casadocodigo.autor
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.QueryValue
 
 @Controller("/autores")
 class BuscaAutoresController(private val autorRepository: AutorRepository) {
 
     @Get
-    fun listar(): HttpResponse<List<DetalhesDoAutorResponse>> {
-        val autores = autorRepository.findAll().toList() // deixa imutável
+    fun listar(@QueryValue(defaultValue = "") email: String): HttpResponse<Any> {
 
-        val response = autores.map { autor ->
-            DetalhesDoAutorResponse(autor)
+        if (email.isBlank()) {
+            val autores = autorRepository.findAll().toList() // deixa imutável
+
+            val response = autores.map { autor ->
+                DetalhesDoAutorResponse(autor)
+            }
+            return HttpResponse.ok(response)
         }
 
-        return HttpResponse.ok(response)
+        val possivelAutor = autorRepository.findByEmail(email)
+
+        if (possivelAutor.isPresent) {
+            return HttpResponse.ok(DetalhesDoAutorResponse(possivelAutor.get()))
+        }
+
+        return HttpResponse.notFound()
     }
+
+    @Get("/funcional")
+    fun listaFuncional(@QueryValue(defaultValue = "") email: String): HttpResponse<Any> {
+
+        email.run {
+            if (isBlank()) {
+                val autores = autorRepository.findAll().toList()
+                autores.map {
+                    DetalhesDoAutorResponse(it)
+                }.run {
+                    return HttpResponse.ok(this)
+                }
+            } else {
+                //return@dados autorRepository.findByEmail(this)
+                val opt = autorRepository.findByEmail(this)
+                if (opt.isPresent) {
+                    return HttpResponse.ok(DetalhesDoAutorResponse(opt.get()))
+                }
+            }
+
+            return HttpResponse.notFound()
+        }
+
+    }
+
 }
